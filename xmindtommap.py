393ea4d42c7efee44456a755fb451cbf92a7ad64
shpp-sgalbur <shpp-sgalbur@programming.kr.ua>
2018-03-27 14:04:52 +0300

    Update xmindtommap.py
    Решает проблему, когда на импортированном из xmind в mmap файле начали работать уже в программе mindjet, и добавили новые заметки.  Предыдущая версия не справлялась, когда часть заметок отображалась в окне note, а часть -нет.

--- xmindtommap.py
+++ xmindtommap.py
@@ -30,25 +30,37 @@ def remfile(text, strFind):
     readingStart = 0
     resStr = ''
     for start in startList: #для каждой позиции списка
-    
+
         #считываем значение
         startVal = start + len(strFind) + len('="')
         endVal = text.find('"',startVal)
         val = text[startVal:endVal]
-        
+        print(startList.index(start))
         
         if text.find(val,endVal) == -1: #если это значение не встречается второй раз
-            if text.find('<xhtml:html xmlns:xhtml="http://www.w3.org/1999/xhtml"/>',endVal) != -1:
-                startEdit = text.find('<',startVal) #находим позицию внесения изменений
-                #
-                resStr = resStr + text[readingStart:startEdit]
-                startDel = text.find('<xhtml:html xmlns:xhtml="http://www.w3.org/1999/xhtml"/>',endVal)
-                endDel = startDel + len('<xhtml:html xmlns:xhtml="http://www.w3.org/1999/xhtml"/>')
-                readingStart = endDel
-                pastStr = '<html xmlns="http://www.w3.org/1999/xhtml"><p>' + val + '</p></html>'
+            startEdit = text.find('<',startVal) #находим позицию внесения изменений                
+            resStr = resStr + text[readingStart:startEdit]
+            
+            
+            
+            stopFind = startEdit + len('<html xmlns="http://www.w3.org/1999/xhtml"><p>')                
+            if text.find('<html xmlns="http://www.w3.org/1999/xhtml"><p>',startEdit,stopFind) == -1:    
+                pastStr = '<html xmlns="http://www.w3.org/1999/xhtml"><p>' + val + '</p></html>'            
                 resStr = resStr + pastStr
+           
+            readingStart = startEdit            
+            
+            stopFind = startEdit + len('<xhtml:html xmlns:xhtml="http://www.w3.org/1999/xhtml"/>')
                 
-                print(text[startDel:endDel])
+            if text.find('<xhtml:html xmlns:xhtml="http://www.w3.org/1999/xhtml"/>',startEdit,stopFind) != -1:
+                startDel = text.find('<xhtml:html xmlns:xhtml="http://www.w3.org/1999/xhtml"/>',startEdit,stopFind)
+                endDel = startDel + stopFind -startEdit
+                readingStart = endDel
+                
+                
+
+                
+                
         
     resStr=resStr+text[readingStart:]
     return resStr
@@ -61,8 +73,9 @@ strtext = myfile.read()
 myfile.close()
 
 strfile = remfile(strtext,'PreviewPlainText')
-print(strfile)
+
 f = open("D:/nets/DocumentRec.xml",'w')
 f.write(strfile)
 f.close()
 
+print("Файл исправлен!")
